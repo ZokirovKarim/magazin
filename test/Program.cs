acfa55a9d3d78a5dea2fde5fa10a2a2b435e3373
Karim <karim.zokirov@mail.ru>
2020-03-21 18:43:52 +0500

    test

--- test/Program.cs
+++ test/Program.cs
@@ -6,7 +6,8 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("rrrr");
         }
     }
 }
