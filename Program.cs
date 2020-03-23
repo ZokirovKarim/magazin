07e073c6ffff0f8e5e6fcce499b0f75bfef963cb
Karim <karim.zokirov@mail.ru>
2020-03-23 16:45:10 +0500

    ДЗ-2

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace zokirov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    double a=16.80,b=12.40;             
+        //    Console.WriteLine( "Задача 1:" + Math.Sqrt(a*b));
+
+        //    double a=1.40, b=-5.50, c=0.6;
+        //   double AC=a+b;
+        //     double BC=b+c;
+        //    Console.WriteLine("Задача 2");
+        //    Console.WriteLine($"AC:{0},BC:{1}"+ AC,BC);
+
+        //    double x1=-6.20, x2=2.10, y1=5.20, y2=9.80;
+        //    double z= Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+        //    Console.WriteLine("Задача 3");
+        //    Console.WriteLine(z); 
+
+            // int j=41;
+            // int k=j%10;
+            // int s= (j-k)/10;
+            // int x=(k*10)+s;
+            // Console.WriteLine("Задача4");
+
+            // Console.WriteLine(x);
+
+            // int n= 10985;
+            // int tima=n/60;
+            // Console.Wri
+        
+            // Console.WriteLine(tima);
+              
+        //       int K=202;
+        //       int n=6;
+        //       int D = ((K+n-2)%7)+1;
+          //  int  e = 202, i = e % 7;
+         // Console.WriteLine("Задача 6");
+          //   Console.WriteLine(i);     
+
+                                 
+            
+         // Console.ReadKey();
+         }
+        
+    }
+}
