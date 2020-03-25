8c534a4006882ca4a1cb0f858c537dca4d3ebbbb
Karim <karim.zokirov@mail.ru>
2020-03-25 16:37:42 +0500

    HJ

--- Program.cs
+++ Program.cs
@@ -6,45 +6,106 @@ namespace zokirov
     {
         static void Main(string[] args)
         {
-        //    double a=16.80,b=12.40;             
-        //    Console.WriteLine( "Задача 1:" + Math.Sqrt(a*b));
+       
 
-        //    double a=1.40, b=-5.50, c=0.6;
-        //   double AC=a+b;
-        //     double BC=b+c;
-        //    Console.WriteLine("Задача 2");
-        //    Console.WriteLine($"AC:{0},BC:{1}"+ AC,BC);
-
-        //    double x1=-6.20, x2=2.10, y1=5.20, y2=9.80;
-        //    double z= Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
-        //    Console.WriteLine("Задача 3");
-        //    Console.WriteLine(z); 
-
-            // int j=41;
-            // int k=j%10;
-            // int s= (j-k)/10;
-            // int x=(k*10)+s;
-            // Console.WriteLine("Задача4");
-
-            // Console.WriteLine(x);
-
-            // int n= 10985;
-            // int tima=n/60;
-            // Console.Wri
-        
-            // Console.WriteLine(tima);
-              
-        //       int K=202;
-        //       int n=6;
-        //       int D = ((K+n-2)%7)+1;
-          //  int  e = 202, i = e % 7;
-         // Console.WriteLine("Задача 6");
-          //   Console.WriteLine(i);     
-
-                                 
+        // ДЗ-3 
+        // Задача №1 Данны две переменные целого тип: А и В. 
+        // int a=44, b=25;
+        //   if (a==b)
+        // {
+        //      a=0;
+        //      b=0;
+        //      Console.WriteLine("A=" +a);
+        //      Console.WriteLine("B="+b);
+        // }
+        // else if(a>b)
+        // {
             
-         // Console.ReadKey();
+        //     Console.Write("A= "+a);
+
+
+        // }
+        
+       
+        // else
+        // {
+            
+        //     Console.Write("B= "+b);
+        // }
+        // Console.ReadKey();
+
+
+        // }
+         
+         // Задание 2 
+        //  int operand1, operand2;
+        //  string sing;
+        //  Console.WriteLine("введите число:   ");
+        //  operand1=int.Parse(Console.ReadLine());
+
+        //  Console.WriteLine("введите число:   ");
+
+        //  operand2=int.Parse(Console.ReadLine());
+        //  Console.WriteLine("Выбор +, - , * , /");
+
+        //  sing=Console.ReadLine();
+        //  switch(sing)
+        //  {
+        //      case"+":Console.WriteLine(operand1+operand2);
+        //      break;
+        //      case "-": Console.WriteLine(operand1-operand2);
+        //      break;
+        //      case"*": Console.WriteLine(operand1*operand2);
+        //      break;
+        //      case "/": 
+        //      if (operand1==0&&operand2==0)
+        //      {
+        //          Console.WriteLine("ошибка");
+        //          break;
+        //      }
+        //      else 
+        //      {
+        //          Console.WriteLine(operand1/operand2);
+        //      }
+        //     break;
+        //     default: Console.WriteLine("ошибка");
+        //     break;
+        //  }
+             
+             
+        //  Console.ReadKey();
+        
+        Console.WriteLine("Выберите 1 то 100");
+          double num = double.Parse(Console.ReadLine());
+        
+         if (num>=0 && num<=14)
+         {
+             Console.WriteLine("[0-14]");
+
          }
+         if (num>=15 && num <=35)
+         {
+             Console.WriteLine("[15-35]");
+         }
+         if (num>=36 && num<=50)
+         {
+             Console.WriteLine("[36-50]");
+
+         }
+         if (num>=51 && num<=100)
+         {
+             Console.WriteLine("[51-100]");
+         }
+         Console.ReadLine();
+
+
+
+        
+
+
+        }
+
+         
         
     }
 }
