b84a4ee481d0509e647cfd57df65126d69033597
Karim <karim.zokirov@mail.ru>
2020-03-26 19:42:28 +0500

    hh

--- Program.cs
+++ Program.cs
@@ -83,38 +83,110 @@ namespace zokirov
              
          
         
-        Console.WriteLine("Выберите 1 то 100");
-          double num = double.Parse(Console.ReadLine());
+        // Console.WriteLine("Выберите 1 то 100");
+        //   double num = double.Parse(Console.ReadLine());
         
-         if (num>=0 && num<=14)
-         {
-             Console.WriteLine("[0-14]");
+        //  if (num>=0 && num<=14)
+        //  {
+        //      Console.WriteLine("[0-14]");
 
-         }
-         if (num>=15 && num <=35)
-         {
-             Console.WriteLine("[15-35]");
-         }
-         if (num>=36 && num<=50)
-         {
-             Console.WriteLine("[36-50]");
+        //  }
+        //  if (num>=15 && num <=35)
+        //  {
+        //      Console.WriteLine("[15-35]");
+        //  }
+        //  if (num>=36 && num<=50)
+        //  {
+        //      Console.WriteLine("[36-50]");
 
-         }
-         if (num>=51 && num<=100)
-         {
-             Console.WriteLine("[51-100]");
-         }
-         else {
-             Console.WriteLine("Нет такого числа");
-         }
-         Console.ReadLine();
+        //  }
+        //  if (num>=51 && num<=100)
+        //  {
+        //      Console.WriteLine("[51-100]");
+        //  }
+        //  else {
+        //      Console.WriteLine("Нет такого числа");
+        //  }
+        //  Console.ReadLine();
 
 
 
+        // string inpuUserLogin="";
+        // string inputUserPassword=""; 
+       
+        // string userAdminRol="Admin";
+        // string userLogin ="sa";
+        // string userPass="1234";
+
+        // string userLoginKarim = "karim";
+        // string userKArim="1233";
+
+
+        // Console.WriteLine("Login");
+        //  inpuUserLogin= Console.ReadLine();
+
+        // Console.WriteLine("Pass");
+        // inputUserPassword=Console.ReadLine();
+
+       
+
+    
+
+        // if (inpuUserLogin.ToLower()==userLogin.ToLower()
+        // && inputUserPassword.ToLower()==userPass.ToLower()
+        // || inpuUserLogin.ToLower()==userKArim.ToLower()&&
+        // inputUserPassword.ToLower()==userKArim.ToLower()
+        // ) 
+        // {
+        //     Console.WriteLine($"Логин:{0}Пароль:{1}Роль:{2}"+userLogin,userPass,userAdminRol)
+        //     ;
+        // }
+        // else 
+        // {
+        //     Console.WriteLine("Не такого пользователя");
+        // }
+        // Console.ReadKey();
         
+        decimal userBalanc= 300;
+        string bac
 
+        Console.WriteLine($"Ваш баланс - {userBalanc}");
+        if (userBalanc<=0) Console.WriteLine("У вас несдостаточно средств");
+        Console.WriteLine(""+
+        "1.Кола - 10 сомон\n"+
+        "2.Сникерс - 7 сомон\n"+
+        "3.Марс - 9 сомон\n"+
+        "4.Чипсы - 4 сомон\n"+
+        "5.Сок - 14 сомон\n"+
+        "6. Выход"+
+        " Ваш Выбор");
 
+        int choice = int.Parse(Console.ReadLine());
+        switch(choice)
+        {
+            case 1:
+            {
+                if (userBalanc-10>=0)
+                basker+=
+            }
         }
+        
+        Console.ReadKey();
+
+        
+
+
+
+
+
+      
+
+        
+
+
+
+
+         }
 
          
         
