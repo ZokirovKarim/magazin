6927d09d168ca04f9d637e87b320cfe786d38f6a
Karim <karim.zokirov@mail.ru>
2020-03-28 12:18:03 +0500

    DZ

--- Program.cs
+++ Program.cs
@@ -6,12 +6,138 @@ namespace zokirov
     {
         static void Main(string[] args)
         {
-        
+        //     double percent, summa;
+        //     summa=Convert.ToDouble(Console.ReadLine());
+        //     if (summa>=500&&summa<1000)
+        //     {
+       
+        //         percent=97*summa/100;
+        //         Console.WriteLine($"покупка с %^{percent}");
+        //     }
+        //     if (summa>=1000)
+        //     {
+        //         percent=95*summa/100;
+        //         Console.WriteLine($"покупка с %^{percent}");
+                               
 
+        //     }
+        //     if(summa<500)
+        //     {
+        //         Console.WriteLine(summa);
+        //     }
 
-         }
+        // }
+       // int a = 11, b = 66, c = 7, d = 9;
 
-         
+            // if(a < b && b < c && c < d){
+            //     Console.WriteLine("Числа расположены по возрастанию");
+            // }
+            // else if(a == b && b == c && c == d){
+            //     Console.WriteLine("Произведение значении = " + (a * b * c * d));
+            // }
+            // else if(a <= b && a <= c && a <= d)   
+            // {
+            //     Console.WriteLine($"Min:{a}");
+            // }
+            // else if(b <= a && b <= c && b <= d){
+
+            //     Console.WriteLine($"Min:{b}");
+            // }
+            // else if(c <= a && c <= b && c <= d){
+            //     Console.WriteLine($"Min:{c}");
+            // }
+            // else if(d <= a && d <= b && d <= c){
+            //     Console.WriteLine($"Min:{d}");
+            // }
+
+// int a = int.Parse(Console.ReadLine()), b = int.Parse(Console.ReadLine()), c = int.Parse(Console.ReadLine()),d,e,f;
+           
+        //     if(a < b && b < c ){
+        //         d = a;
+        //         a = c;
+        //         b = b;
+        //         c = d;
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+        //     else if(a > b && b > c){
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+        //     else if(a>b && b<c){
+        //         d = a;
+        //         e = b;
+        //         f = c;
+
+        //         a = d;
+        //         b = f;
+        //         c = e;
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+
+        //     else if(a < b && b > c && a < c){
+        //         d = a;
+        //         e = b;
+        //         f = c;
+
+        //         a = e;
+        //         b = f;
+        //         c = d;
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+        //     else if(a < b && b > c && a > c){
+        //         d = a;
+        //         e = b;
+        //         f = c;
+
+        //         a = e;
+        //         b = d;
+        //         c = f;
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+        //     else if(a > b && b < c && a > c){
+        //         d = a;
+        //         e = b;
+        //         f = c;
+
+        //         a = d;
+        //         b = f;
+        //         c = e;
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+
+        //     else if(a <= b && b < c){ 
+        //         d = a;
+        //         e = b;
+        //         f = c;
+
+        //         a = f;
+        //         b = e;
+        //         c = d;
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+
+        //     else if(a < b && b >= c){ 
+        //         d = a;
+        //         e = b;
+        //         f = c;
+
+        //         a = f;
+        //         b = e;
+        //         c = d;
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+        //     else if(a <= b && b >= c){ 
+        //         d = a;
+        //         e = b;
+        //         f = c;
+
+        //         a = f;
+        //         b = e;
+        //         c = d;
+        //         Console.WriteLine($"{a} >= {b} >= {c}");
+        //     }
+             
+
+        }
           
     }
 }
